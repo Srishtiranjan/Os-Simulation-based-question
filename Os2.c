c29cfd90c98c1434ba2f5845c014cca642c50f0c
Srishtiranjan <42888081+Srishtiranjan@users.noreply.github.com>
2019-04-10 20:54:19 +0530

    Create Os2.c

---
+++ Os2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int arr[]=10,n,i,j;
+printf("Enter the number of gifts");
+for(i=0;i<10;i++)
+{
+scanf("%d",&arr[i]);
+}
+for (i=0;i<10;i++)
+{
+ for(j=0;j<10;j++)
+ {
+ if(arr[i]< arr[i+1])
+  {
+   n=arr[i+1];
+   arr[i+1]=arr[i];
+   arr[i]=n;
+   }
+  }
+ }
+ for(i=0;i<10;i++)
+ {
+ printf("The s%d item is:-%d",i,arr[i]);
+ }
+ }
+ 
